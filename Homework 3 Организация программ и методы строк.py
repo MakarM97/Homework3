981a9030202fbef8e9dff3b46ba156e860da01cb
MakarM97 <165716716+MakarM97@users.noreply.github.com>
2024-04-04 20:14:01 +1000

    Add files via upload

--- Homework 3 Организация программ и методы строк.py
+++ Homework 3 Организация программ и методы строк.py
@@ -2,7 +2,7 @@ name = input('hello')
 print(len(name))
 print('hello'.upper())
 print('hello'.lower())
-print('hello'.replace(' ', '2'))
+print('hello'.replace(' ', ''))
 name_1 = 'hello'
 print(name_1[0])
 print(name_1[-1])
